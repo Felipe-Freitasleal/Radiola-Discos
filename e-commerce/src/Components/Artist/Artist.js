3ca5cc7b203fe790e5e0566e3980c19e9eb345cf
Felipe-Freitasleal <felipeleal094@gmail.com>
2022-12-08 14:04:55 -0300

    Primeiras estilizações no componente Disco.

--- e-commerce/src/Components/Artist/Artist.js
+++ e-commerce/src/Components/Artist/Artist.js
@@ -3,11 +3,10 @@ import {
     Center,
     useColorModeValue,
     Heading,
-    Text,
-    Stack,
     Image,
     Button
 } from '@chakra-ui/react';
+import { CiMusicNote1 } from "react-icons/ci";
 
 const IMAGE =
     'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
@@ -16,66 +15,48 @@ function Artist() {
     return (
         <Center py={12}>
             <Box
+                display={'flex'}
+                flexDir={'column'}
                 role={'group'}
-                p={6}
+                p={2}
                 maxW={'330px'}
+                minW={'330px'}
+                maxH={'500px'}
+                minH={'500px'}
                 w={'full'}
                 bg={useColorModeValue('white', 'gray.800')}
                 boxShadow={'2xl'}
                 rounded={'lg'}
                 pos={'relative'}
-                zIndex={1}>
-                <Box
+                zIndex={1}
+                _hover={{
+                    boxShadow: '0px 1px 12px -5px black'
+                }}
+                alignItems={'center'}
+                justifyContent={'space-evenly'}>
+                <Image
                     rounded={'lg'}
-                    mt={-12}
-                    pos={'relative'}
-                    height={'230px'}
-                    _after={{
-                        transition: 'all .3s ease',
-                        content: '""',
-                        w: 'full',
-                        h: 'full',
-                        pos: 'absolute',
-                        top: 5,
-                        left: 0,
-                        backgroundImage: `url(${IMAGE})`,
-                        filter: 'blur(15px)',
-                        zIndex: -1,
-                    }}>
-                    <Image
-                        rounded={'lg'}
-                        height={230}
-                        width={282}
-                        objectFit={'cover'}
-                        src={IMAGE}
-                    />
-                </Box>
-                <Stack pt={10} align={'center'}>
-                    <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-                        NOME DA BANDA AQUI!
-                    </Heading>
-                    <Stack direction={'column'} align={'center'}>
-                        <Text fontWeight={800} fontSize={'xl'}>
-                            Quantos Albuns: 3
-                        </Text>
-                        <Button
-                            px={4}
-                            fontSize={'sm'}
-                            rounded={'full'}
-                            bg={'white.400'}
-                            color={'black'}
-                            boxShadow={
-                                '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-                            }
-                            _hover={{
-                                bg: 'blue.400',
-                                color: 'white'
-                            }}
-                            >
-                            Ver Albuns
-                        </Button>
-                    </Stack>
-                </Stack>
+                    height={230}
+                    maxH={230}
+                    minH={230}
+                    width={282}
+                    maxW={282}
+                    minW={282}
+                    objectFit={'cover'}
+                    src={IMAGE}
+                />
+                <Heading
+                    fontSize={'2xl'}
+                    fontFamily={'body'}
+                    fontWeight={500}
+                    textAlign={'center'}
+                    m={1}
+                >
+                    A Banda Mais bonita da Cidade
+                </Heading>
+                <Button variant='solid' bg='darkgrey' color={'black'} leftIcon={<CiMusicNote1/>}>
+                    Ver Albuns
+                </Button>
             </Box>
         </Center>
     );
--- e-commerce/src/Components/Disco/Disco.js
+++ e-commerce/src/Components/Disco/Disco.js
@@ -1,77 +1,45 @@
 import {
-  Box,
-  Center,
-  useColorModeValue,
-  Heading,
-  Text,
+  Card,
   Stack,
+  CardBody,
+  CardFooter,
+  Heading,
   Image,
+  Button,
+  Text
 } from '@chakra-ui/react';
 
-const IMAGE =
-  'https://images.unsplash.com/photo-1518051870910-a46e30d9db16?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1350&q=80';
-
-function Disco () {
+function Disco() {
   return (
-    <Center py={12}>
-      <Box
-        role={'group'}
-        p={6}
-        maxW={'330px'}
-        w={'full'}
-        bg={useColorModeValue('white', 'gray.800')}
-        boxShadow={'2xl'}
-        rounded={'lg'}
-        pos={'relative'}
-        zIndex={1}>
-        <Box
-          rounded={'lg'}
-          mt={-12}
-          pos={'relative'}
-          height={'230px'}
-          _after={{
-            transition: 'all .3s ease',
-            content: '""',
-            w: 'full',
-            h: 'full',
-            pos: 'absolute',
-            top: 5,
-            left: 0,
-            backgroundImage: `url(${IMAGE})`,
-            filter: 'blur(15px)',
-            zIndex: -1,
-          }}
-          _groupHover={{
-            _after: {
-              filter: 'blur(20px)',
-            },
-          }}>
-          <Image
-            rounded={'lg'}
-            height={230}
-            width={282}
-            objectFit={'cover'}
-            src={IMAGE}
-          />
-        </Box>
-        <Stack pt={10} align={'center'}>
-          <Text color={'gray.500'} fontSize={'sm'} textTransform={'uppercase'}>
-            Brand
-          </Text>
-          <Heading fontSize={'2xl'} fontFamily={'body'} fontWeight={500}>
-            Nice Chair, pink
+    <Card
+      direction={{ base: 'column', sm: 'row' }}
+      overflow='hidden'
+      variant='outline'
+    >
+      <Image
+        objectFit='cover'
+        maxW={{ base: '100%', sm: '200px' }}
+        src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+        alt='Caffe Latte'
+      />
+
+      <Stack>
+        <CardBody>
+          <Heading size='md'>
+            Nome do CD
           </Heading>
-          <Stack direction={'row'} align={'center'}>
-            <Text fontWeight={800} fontSize={'xl'}>
-              $57
-            </Text>
-            <Text textDecoration={'line-through'} color={'gray.600'}>
-              $199
-            </Text>
-          </Stack>
-        </Stack>
-      </Box>
-    </Center>
+
+          <Text py='2'>
+            Faixas
+          </Text>
+        </CardBody>
+        <CardFooter>
+          <Button variant='solid' bg='darkgrey' color={'black'}>
+            Add ao Carrinho
+          </Button>
+        </CardFooter>
+      </Stack>
+    </Card>
   );
 }
 
